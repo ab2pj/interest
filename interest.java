ef2c0221b1e90fbfe5045f396870d22dfe8742eb
Bryant Bigelow <45375291+ab2pj@users.noreply.github.com>
2022-08-10 20:18:39 -0400

    Update interest.java

--- interest.java
+++ interest.java
@@ -1,1 +1,23 @@
+// create data entry window that will calculate missing values for interest rate calculations 
 
+import java.awt.*
+import java.awt.event.*
+
+public class Interest extends Frame {
+  
+  public Interest() {
+    //Anonymous inner class to handle window close events
+    addWindowListener(new WindowAdapter() {
+      public void WindowClosing(WindowEvent we) {
+        System.exit(0);
+      }
+    });
+    
+    //Anonymous inner class to handle mouse click events
+    addMouseListener(new MouseAdapter() {
+      public void MouseClicked(me) {
+      
+      }
+    });
+  }  
+}  
