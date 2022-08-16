7be6aa894b4e099ea1ecce34d362e2d54970b886
Bryant Bigelow <45375291+ab2pj@users.noreply.github.com>
2022-08-16 14:59:02 -0400

    Update interest.java

--- interest.java
+++ interest.java
@@ -4,6 +4,10 @@ import java.awt.*
 import java.awt.event.*
 
 public class Interest extends Frame {
+  final int inc = 25;
+  int max = 500;
+  int min = 200;
+  Dimension d;
   
   public Interest() {
     //Anonymous inner class to handle window close events
@@ -16,8 +20,25 @@ public class Interest extends Frame {
     //Anonymous inner class to handle mouse click events
     addMouseListener(new MouseAdapter() {
       public void MouseClicked(me) {
-      
+        int w = (d.width + inc) > max ? min : (d.width + inc);
+        int h = (d.height + inc) > max ? min : (d.height + inc);
+        setSize(new Dimension (w,h));
       }
     });
+  }
+  
+  public void paint(Graphics g) {
+    Insets i = getInsets();
+    d = getSize();
+    
+    g.drawLine(i.left, i.top, d.width-i.right, d.height-i.bottom);
+    g.drawLine(i.left, d.height-i.bottom, d.width-i.right, i.top);
+  }
+  
+  public static void main(String[] args {
+    Interest appwin = new Interest();
+    appwin.setSize (new Dimension(200,200));
+    appwin.setTitle ("Interest");
+    appwin.setVisible (true);
   }  
 }  
